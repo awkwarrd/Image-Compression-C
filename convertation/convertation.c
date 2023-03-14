2233b0279434a965e121e8794a3f466ea2f4d159
awkward <heregoesnothingowo@gmail.com>
2023-03-14 15:59:50 +0100

    convertation fixed

--- convertation/convertation.c
+++ convertation/convertation.c
@@ -2,7 +2,6 @@
 
 #include "convertation.h"
 
-
 void convertation(char* filename)
 {
     FILE *in = fopen(filename, "rb");
@@ -14,27 +13,8 @@ void convertation(char* filename)
         return;
     }
     short x;
-    short st = 1;
-    short result = 0;
     while ((x = fgetc(in)) != EOF)
-    {
-        for (int i = 0; i <= 7; i++)
-        {
-            if (x & (1 << (7 - i)))
-            {
-                fputc('1', out);
-                result += st;
-            }
-            else fputc('0', out);
-            st *= 2;
-        }
-        st = 1;
-        printf("%d ", result);
-        //fputc((char)result, out);
-        result = 0;
-        
-        //fputc((char)x,out);
-    }
+       fputc((char)x,out);
     fclose(in);
     fclose(out);
     return ;   
